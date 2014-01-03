46db5304e6e7285be70577fdaa0e494f7a86d8d5
Craig Maslowski <craig@craigmaslowski.com>
2014-01-03 14:41:26 -0800

    added banner to gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function(grunt) {
+	var banner = '/*\n<%= pkg.name %> <%= pkg.version %> - <%= pkg.description %>\n<%= pkg.repository.url %>\nBuilt on <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n';
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 	});
