3469adf37ba7db3508ed125fc2b04324f2c3cea1
Craig Maslowski <craig@craigmaslowski.com>
2014-01-03 21:50:35 -0800

    added jshint and config

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,20 @@
 module.exports = function(grunt) {
-	var banner = '/*\n<%= pkg.name %> <%= pkg.version %> - <%= pkg.description %>\n<%= pkg.repository.url %>\nBuilt on <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n';
+	var banner = '/*\n<%= pkg.name %> <%= pkg.version %>';
+	banner += '- <%= pkg.description %>\n<%= pkg.repository.url %>\n';
+	banner += 'Built on <%= grunt.template.today("yyyy-mm-dd") %>\n*/\n';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ['gruntfile.js', 'src/*.js'],
+			options: {
+				maxlen: 80,
+				quotmark: 'single'
+			}
+		}
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.registerTask('default', ['jshint']);
 };
